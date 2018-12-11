fb21b43d7544a27db77266dd3f3873df56cf6c96
pxgamer <owzie123@gmail.com>
2018-12-11 11:44:06 +0000

    Update poolsanity to follow PSR-2

--- poolsanity.php
+++ poolsanity.php
@@ -45,43 +45,49 @@ while (1) {
         $db->run("UPDATE miners SET historic=historic*0.95+shares, shares=0,bestdl=1000000");
         $db->run("TRUNCATE table nonces");
 
-           $r=$db->run("SELECT * FROM miners WHERE historic>0");
-                $total_hr=0;
-                $total_gpu=0;
-                foreach($r as $x){
-                        $thr=$db->row("SELECT SUM(hashrate) as cpu, SUM(gpuhr) as gpu FROM workers WHERE miner=:m AND updated>UNIX_TIMESTAMP()-3600",array(":m"=>$x['id']));
-                        if($x['historic']/$thr['cpu']<2||$x['historic']/$thr['gpu']<2) {
-                                $thr['cpu']=0;
-                                $thr['gpu']=0;
-                                echo "$x[id] [$x[historic]] -> ".$x['historic']/$thr[cpu]."\n";
-                        }
-                        $total_hr+=$thr['cpu'];
-                        $total_gpu+=$thr['gpu'];
-                }
-            echo "Total hr: $total_hr\n";
-            $db->run("UPDATE info SET val=:thr WHERE id='total_hash_rate'",array(":thr"=>$total_hr));
-            $db->run("UPDATE info SET val=:thr WHERE id='total_gpu_hr'",array(":thr"=>$total_gpu));
-
+        $r = $db->run("SELECT * FROM miners WHERE historic>0");
+        $total_hr = 0;
+        $total_gpu = 0;
+        foreach ($r as $x) {
+            $thr = $db->row(
+                "SELECT SUM(hashrate) as cpu, SUM(gpuhr) as gpu FROM workers WHERE miner=:m AND updated>UNIX_TIMESTAMP()-3600",
+                [":m" => $x['id']]
+            );
+            if ($x['historic'] / $thr['cpu'] < 2 || $x['historic'] / $thr['gpu'] < 2) {
+                $thr['cpu'] = 0;
+                $thr['gpu'] = 0;
+                echo "$x[id] [$x[historic]] -> ".$x['historic'] / $thr[cpu]."\n";
+            }
+            $total_hr += $thr['cpu'];
+            $total_gpu += $thr['gpu'];
+        }
+        echo "Total hr: $total_hr\n";
+        $db->run("UPDATE info SET val=:thr WHERE id='total_hash_rate'", [":thr" => $total_hr]);
+        $db->run("UPDATE info SET val=:thr WHERE id='total_gpu_hr'", [":thr" => $total_gpu]);
     }
 
-    if ($current % 2) $max_dl = $pool_config['max_deadline_gpu']; else $max_dl = $pool_config['max_deadline']; 
+    if ($current % 2) {
+        $max_dl = $pool_config['max_deadline_gpu'];
+    } else {
+        $max_dl = $pool_config['max_deadline'];
+    }
 
-    
+
     $cache_file = "cache/info.txt";
 
     $f = file_get_contents($pool_config['node_url']."/mine.php?q=info");
     $g = json_decode($f, true);
 
     $res = [
-        "difficulty"    => $g['data']['difficulty'],
-        "block"         => $g['data']['block'],
-        "height"        => $g['data']['height'],
-        "public_key"    => $pool_config['public_key'],
-        "limit"         => $max_dl,
-        "recommendation"=> $g['data']['recommendation'], 
-        "argon_mem"     => $g['data']['argon_mem'],  
-        "argon_threads" => $g['data']['argon_threads'], 
-        "argon_time"    => $g['data']['argon_time'],
+        "difficulty" => $g['data']['difficulty'],
+        "block" => $g['data']['block'],
+        "height" => $g['data']['height'],
+        "public_key" => $pool_config['public_key'],
+        "limit" => $max_dl,
+        "recommendation" => $g['data']['recommendation'],
+        "argon_mem" => $g['data']['argon_mem'],
+        "argon_threads" => $g['data']['argon_threads'],
+        "argon_time" => $g['data']['argon_time'],
     ];
     $fin = json_encode(["status" => "ok", "data" => $res, "coin" => "arionum"]);
     echo "\n$fin\n";
