7c53726fe6432fe9d3044d9171ca0b4449c96dca
Ario <dev@ariochain.info>
2019-02-15 13:22:49 +0000

    Change api.php - m changed to id

--- api.php
+++ api.php
@@ -3,9 +3,9 @@
 Usage: 
 
 /api?q=status			returns current status of the pool
-/api?q=miner&m=<wallet>		returns total hashrate and shares of miner
-/api?q=payments&m=<wallet>	returns payments of miner
-/api?m=<wallet>			returns total hashrate and shares and payments of miner
+/api?q=miner&id=<wallet>		returns total hashrate and shares of miner
+/api?q=payments&id=<wallet>	returns payments of miner
+/api?id=<wallet>			returns total hashrate and shares and payments of miner // do we need this, want this?
 /api.php without flags 		also returns pool status at the moment for compatibility reasons
 
 */
@@ -18,20 +18,19 @@ $q = san($_GET['q']);
 
 if ($q == "miner") {
 
-	$m = san($_GET['m']);
+	$id = san($_GET['id']);
 
-	if ($m == null) {
+	if ($id == null) {
 
-			echo "Invalid request";
+			api_err("Invalid request");
 
 		} else {
 
-			$miner=$m;
-			$hashrate=$db->single("SELECT hashrate FROM miners WHERE id='$m'");
-			$gpu_hr=$db->single("SELECT gpuhr FROM miners WHERE id='$m'");
-			$historic=$db->single("SELECT historic FROM miners WHERE id='$m'");
-			$shares=$db->single("SELECT (shares FROM miners WHERE id='$m'");
-			$update=$db->single("SELECT updated FROM MINERS where id='$m'");
+			$hashrate=$db->single("SELECT hashrate FROM miners WHERE id=:id",[":id"=>$id]);
+			$gpu_hr=$db->single("SELECT gpuhr FROM miners WHERE id=:id",[":id"=>$id]);
+			$historic=$db->single("SELECT historic FROM miners WHERE id=:id",[":id"=>$id]);
+			$shares=$db->single("SELECT (shares FROM miners WHERE id=:id",[":id"=>$id]);
+			$update=$db->single("SELECT updated FROM miners WHERE id=:id",[":id"=>$id]);
 				if ($update == false) {
 					$update = "No nonce submitted";
 				}
@@ -48,7 +47,7 @@ if ($q == "miner") {
 					$gpu_hr = "0";
 				}
 
-			echo json_encode(array("miner"=>$miner, "cpu_hr"=>$hashrate, "gpu_hr"=>$gpu_hr, "historic shares"=>$historic, "current shares"=>$shares, "last nonce submitted"=>$update));
+			echo json_encode(array("miner"=>$id, "cpu_hr"=>$hashrate, "gpu_hr"=>$gpu_hr, "historic shares"=>$historic, "current shares"=>$shares, "last nonce submitted"=>$update));
 		}
 
 	}
@@ -78,39 +77,42 @@ elseif ($q == "status") {
 	
 	} elseif ($q == "payments") { 
 
-	$m = san($_GET['m']);
+		$id = san($_GET['id']);
 
-	if ($m == null) {
-                echo "Invalid request";
-        } else {
-			$yesterday=time()-86400;
-			$yesterday_block=$aro->single("SELECT height+1 FROM blocks WHERE date<=$yesterday ORDER by height DESC LIMIT 1");
-			$last_payment_txn=$db->single("SELECT txn FROM payments WHERE address='$m' AND done=1 ORDER by height DESC LIMIT 1");
-			$last_payment_time=$aro->single("SELECT date FROM transactions WHERE id=$last_payment_txn");
-			$miner=$m;
-			$total_paid=$db->single("SELECT total_paid FROM miners WHERE id='$m'");
-			$pending=$db->single("SELECT pending FROM miners WHERE id='$m'");
-			$last_payment=$db->single("SELECT SUM(val) FROM payments WHERE txn=:lasttxn AND done=1",[":lasttxn"=>$last_payment_txn]);
+		if ($id == null) {
 
-			if ($last_payment_time == false) {
-				$last_payment_time = "Payment in process";
+			api_err("Invalid request");
+		
+		} else {
+				$yesterday=time()-86400;
+				$yesterday_block=$aro->single("SELECT height+1 FROM blocks WHERE date<=$yesterday ORDER by height DESC LIMIT 1");
+				$last_payment_txn=$db->single("SELECT txn FROM payments WHERE address=:id AND done=1 ORDER by height DESC LIMIT 1",[":id"=>$id]);
+				$last_payment_time=$aro->single("SELECT date FROM transactions WHERE id=$last_payment_txn");
+				$miner=$m;
+				$total_paid=$db->single("SELECT total_paid FROM miners WHERE id=:id",[":id"=>$id]);
+				$pending=$db->single("SELECT pending FROM miners WHERE id=:id",[":id"=>$id]);
+				$last_payment=$db->single("SELECT SUM(val) FROM payments WHERE txn=:lasttxn AND done=1",[":lasttxn"=>$last_payment_txn]);
+
+				if ($last_payment_time == false) {
+					$last_payment_time = "Payment in process";
+				}
+
+				if ($last_payment == 0 ) {
+					$last_payment_time = "No payment yet";
+				}
+
+				$past_24h=$db->single("SELECT SUM(val) FROM payments WHERE address=:id AND height>=$yesterday_block AND done=1",[":id"=>$id]);
+
+				echo json_encode(array("miner"=>$miner, "total paid"=>$total_paid, "pending"=>$pending, "past_24h"=>$past_24h, "last_payment"=>$last_payment, "last_payment_date"=>$last_payment_time));
 			}
-
-			if ($last_payment == 0 ) {
-				$last_payment_time = "No payment yet";
-			}
-
-			$past_24h=$db->single("SELECT SUM(val) FROM payments WHERE address='$m' AND height>=$yesterday_block AND done=1");
-			echo json_encode(array("miner"=>$miner, "total paid"=>$total_paid, "pending"=>$pending, "past_24h"=>$past_24h, "last_payment"=>$last_payment, "last_payment_date"=>$last_payment_time));
 		}
-	}
 
 
-else {
+	else {
 
-		$m = san($_GET['m']);
+		$id = san($_GET['id']);
 
-        if ($m == null) {
+        if ($id == null) {
 
 			// we keep this here as this is the old aropool.com api, for compatibility
 			$total_hr=$db->single("SELECT val FROM info WHERE id='total_hash_rate'");
@@ -123,12 +125,11 @@ else {
 		
 		} else {
 
-			$miner=$m;
-			$hashrate=$db->single("SELECT hashrate FROM miners WHERE id='$m'");
-			$gpu_hr=$db->single("SELECT gpuhr FROM miners WHERE id='$m'");
-			$historic=$db->single("SELECT historic FROM miners WHERE id='$m'");
-			$shares=$db->single("SELECT shares FROM miners WHERE id='$m'");
-			$update=$db->single("SELECT updated FROM MINERS where id='$m'");
+			$hashrate=$db->single("SELECT hashrate FROM miners WHERE id=:id",[":id"=>$id]);
+			$gpu_hr=$db->single("SELECT gpuhr FROM miners WHERE id=:id",[":id"=>$id]);
+			$historic=$db->single("SELECT historic FROM miners WHERE id=:id",[":id"=>$id]);
+			$shares=$db->single("SELECT shares FROM miners WHERE id=:id",[":id"=>$id]);
+			$update=$db->single("SELECT updated FROM miners WHERE id=:id",[":id"=>$id]);
 
 			if ($update == false) {
 				$update = "No nonce submitted";
@@ -152,12 +153,12 @@ else {
 		
 			$yesterday=time()-86400;
 			$yesterday_block=$aro->single("SELECT height+1 FROM blocks WHERE date<=$yesterday ORDER by height DESC LIMIT 1");
-			$last_payment_txn=$db->single("SELECT txn FROM payments WHERE address='$m' AND done=1 ORDER by height DESC LIMIT 1");
+			$last_payment_txn=$db->single("SELECT txn FROM payments WHERE address=:id AND done=1 ORDER by height DESC LIMIT 1",[":id"=>$id]);
 			$last_payment_time=$aro->single("SELECT date FROM transactions WHERE id=$last_payment_txn");
-			$total_paid=$db->single("SELECT total_paid FROM miners WHERE id='$m'");
-			$pending=$db->single("SELECT pending FROM miners WHERE id='$m'");
+			$total_paid=$db->single("SELECT total_paid FROM miners WHERE id=:id",[":id"=>$id]);
+			$pending=$db->single("SELECT pending FROM miners WHERE id=:id",[":id"=>$id]);
 			$last_payment=$db->single("SELECT SUM(val) FROM payments WHERE txn=:lasttxn AND done=1",[":lasttxn"=>$last_payment_txn]);
-
+			
 			if ($last_payment_time == false) {
 				$last_payment_time = "Payment in process";
 			}
@@ -165,10 +166,10 @@ else {
 			if ($last_payment == 0 ) {
 				$last_payment_time = "No payment yet";
 			}
-			
-			$past_24h=$db->single("SELECT SUM(val) FROM payments WHERE address='$m' AND height>=$yesterday_block AND done=1");
 
-			echo json_encode(array("miner"=>$miner, "cpu_hr"=>$hashrate, "gpu_hr"=>$gpu_hr, "historic shares"=>$historic, "current shares"=>$shares, "last nonce submite"=>$update, "total paid"=>$total_paid, "pending"=>$pending, "past_24h"=>$past_24h, "last_payment"=>$last_payment, "last_payment_date"=>$last_payment_time));
+			$past_24h=$db->single("SELECT SUM(val) FROM payments WHERE address=:id AND height>=$yesterday_block AND done=1",[":id"=>$id]);
+
+			echo json_encode(array("miner"=>$id, "cpu_hr"=>$hashrate, "gpu_hr"=>$gpu_hr, "historic shares"=>$historic, "current shares"=>$shares, "last nonce submited"=>$update, "total paid"=>$total_paid, "pending"=>$pending, "past_24h"=>$past_24h, "last_payment"=>$last_payment, "last_payment_date"=>$last_payment_time));
 			
 		}
 
