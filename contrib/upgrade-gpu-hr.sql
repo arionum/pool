a1b8376e08a48e47a681031ecdafd289937522bc
F0nzy <online.jobs.news@gmail.com>
2019-06-13 15:02:19 +0000

    update database

--- contrib/upgrade-gpu-hr.sql
+++ contrib/upgrade-gpu-hr.sql
@@ -1,3 +1,6 @@
 ALTER TABLE `miners` ADD `gpuhr` INT NULL DEFAULT '0' AFTER `hashrate`; 
 ALTER TABLE `workers` ADD `gpuhr` INT NULL DEFAULT '0' AFTER `hashrate`; 
 INSERT INTO `info` (`id`, `val`) VALUES ('total_gpu_hr', '');
+
+ALTER TABLE `workers` ADD `workername` varchar(32) NULL AFTER `id`;
+
